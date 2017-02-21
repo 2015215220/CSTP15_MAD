f1745a1d5eb8d6ed183eb23dd063d61f2cacd2b6
czldl <czldl@tom.com>
2017-02-21 14:29:36 +0800

    创建accountMS的简单版实现

---
+++ gen/cn/edu/chzu/xxxy/sp15/ldl/accountms/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.edu.chzu.xxxy.sp15.ldl.accountms;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
